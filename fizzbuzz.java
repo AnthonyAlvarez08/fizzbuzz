24ba2ec97529473ede28ca388930fea0829d87c3
wataflai <67923669+AnthonyAlvarez08@users.noreply.github.com>
2021-12-10 21:41:38 -0500

    no more wasted space

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -3,17 +3,14 @@ class fizzbuzz {
         String word;
         for (int t = 1; t < 101; ++t) {
             word = "";
-            if (t % 3 == 0) {
+            if (t % 3 == 0) 
                 word += "Fizz";
-            }
-            if (t % 5 == 0) {
+            if (t % 5 == 0)
                 word += "Buzz";
-            }
-            if (word == "") {
+            if (word == "")
                 System.out.println(t);
-            } else {
+            else
                 System.out.println(word);
-            }
         }
     }
 }
